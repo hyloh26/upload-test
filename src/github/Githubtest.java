2b8e2b98d4c61b96dfa01513337b39e538bba64a
user <user@DESKTOP-3C8JB53>
2026-08-27 17:51:41 +0900

    비인가 레파지토리

--- src/github/Githubtest.java
+++ src/github/Githubtest.java
@@ -5,7 +5,7 @@ public class Githubtest {
         // DLP Test Credentials & PII
         String awsAccessKey = "AKIAIOSFOsstestEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
         String githubToken  = "guvFFFxyz";
-        String rsaPrivateKey = "-N RSA PAATEST...KND RSA PRIVATE KEY-----";
+        String rsaPrivateKey = "-N ..A PRIVATE KEY-----";
 
         String testRRN  = "900101-1234567";
         String testPhone = "010-1111-5678";
